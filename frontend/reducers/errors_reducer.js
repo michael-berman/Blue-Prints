149ef0faa989ae917b8bb20f834b331d1c1fa4db
Michael Berman <michael.v.berman@gmail.com>
2018-01-02 12:43:57 -0500

    finish reducers for front auth

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import session from './session_errors_reducer';
+
+//Add other errors to here once project and comments come up
+
+export default combineReducers({
+  session
+})
