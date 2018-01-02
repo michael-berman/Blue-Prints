9559e0a7b16312127748b88eef2ae2d29102e57b
Michael Berman <michael.v.berman@gmail.com>
2018-01-02 13:37:23 -0500

    Add provider, hashrouter to root and root component to entry file

--- frontend/blue_prints.jsx
+++ frontend/blue_prints.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import * as SessionAPIUtil from './util/session_api_util';
+import Root from './components/root';
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   const store = configureStore();
 
-  
+  // Testing
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.login = SessionAPIUtil.login;
   window.logout = SessionAPIUtil.logout;
   window.signup = SessionAPIUtil.signup;
+  // end of testing
 
-  ReactDOM.render(<h1>Welcome to Blue Prints</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
