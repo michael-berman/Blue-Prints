3fc753dfc573da970851bf38f03c6f7eb533b991
Michael Berman <michael.v.berman@gmail.com>
2018-01-02 12:17:55 -0500

    ajax requests work

--- frontend/blue_prints.jsx
+++ frontend/blue_prints.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import * as SessionAPIUtil from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Blue Prints</h1>, root);
 });
+
+
+// window.login = SessionAPIUtil.login;
+// window.logout = SessionAPIUtil.logout;
+// window.signup = SessionAPIUtil.signup;
--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,29 +1,29 @@
 
 export const signup = user => {
-  return {
+  return (
     $.ajax({
       method: 'post',
       url: 'api/users',
       data: { user }
     })
-  };
+  );
 };
 
 export const login = user => {
-  return {
+  return (
     $.ajax({
       method: 'post',
       url: 'api/session',
       data: { user }
     })
-  };
+  );
 };
 
 export const logout = () => {
-  return {
+  return (
     $.ajax({
       method: 'delete',
       url: 'api/session'
     })
-  };
+  );
 };
