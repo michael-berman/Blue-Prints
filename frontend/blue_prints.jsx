bd76c5e6063d77ec686c028061c4d8dcbea4614a
Michael Berman <michael.v.berman@gmail.com>
2018-01-02 13:34:05 -0500

    Add root and app component

--- frontend/blue_prints.jsx
+++ frontend/blue_prints.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-// import * as SessionAPIUtil from './util/session_api_util';
+import * as SessionAPIUtil from './util/session_api_util';
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   const store = configureStore();
+
+  
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  window.login = SessionAPIUtil.login;
+  window.logout = SessionAPIUtil.logout;
+  window.signup = SessionAPIUtil.signup;
+
   ReactDOM.render(<h1>Welcome to Blue Prints</h1>, root);
 });
-
-
-// window.login = SessionAPIUtil.login;
-// window.logout = SessionAPIUtil.logout;
-// window.signup = SessionAPIUtil.signup;
---
+++ frontend/components/root.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+const Root = ({ store }) => {
+  return (
+    <Provider store={store}>
+
+    </Provider>
+  )
+}
