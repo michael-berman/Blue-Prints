f35f8da78104b4d514c6aafe6fca584de4bf35bc
Michael Berman <michael.v.berman@gmail.com>
2018-01-02 12:50:46 -0500

    tested sample state for no current user

--- frontend/blue_prints.jsx
+++ frontend/blue_prints.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 // import * as SessionAPIUtil from './util/session_api_util';
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Welcome to Blue Prints</h1>, root);
 });
 
