e8bff3be47d0eb3d7c973b5a930d1ccb7a9c5761
Michael Berman <michael.v.berman@gmail.com>
2018-01-03 12:15:59 -0500

    Add backend routes to projects

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,3 +1,24 @@
 class Api::ProjectsController < ApplicationController
-  
+
+  def index
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.author = current_user
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :body)
+  end
 end
