0c8b7674ab5fa53ceb8c4170f146c3884a721efd
Michael Berman <michael.v.berman@gmail.com>
2018-01-03 12:02:20 -0500

    migrated projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102185823) do
+ActiveRecord::Schema.define(version: 20180103163649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_projects_on_author_id"
+    t.index ["title"], name: "index_projects_on_title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
