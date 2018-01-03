30339b6f283f239e5979de9d122fec751fb474ed
Michael Berman <michael.v.berman@gmail.com>
2018-01-03 18:03:01 -0500

    Fix auth routes and reducer for currentUser slice of state

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import ProjectIndex from '../project_index/project_index';
+import { Link } from 'react-router-dom';
 
 class HomePage extends React.Component {
   constructor(props){
     super(props);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   componentDidMount(){
     this.props.fetchProjects();
   }
 
+  handleLogout(){
+    this.props.logout();
+  }
+
   render(){
     return (
       <main className="homepage-wrapper">
-        <button onClick={this.props.logout}>Logout</button>
-        <ProjectIndex projects={this.props.projects} />
+        <Link to='/signup'>Signup</Link><br />
+        <Link to='/login'>Login</Link><br />
+        <button onClick={this.handleLogout}>Logout</button>
       </main>
     )
   }
 
-
 }
+// <ProjectIndex projects={this.props.projects} />
 
 export default HomePage;
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -10,7 +10,7 @@ const sessionReducer = (state = _nullUser, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return merge({}, action.currentUser);
+      return merge({}, {currentUser: action.currentUser});
     default:
       return state;
   }
