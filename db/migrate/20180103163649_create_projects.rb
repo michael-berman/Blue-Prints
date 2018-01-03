9b54685cbbb2170f9ecd9cce8aaeb9a663ddc74d
Michael Berman <michael.v.berman@gmail.com>
2018-01-03 11:56:30 -0500

    generated model and migration for projects

---
+++ db/migrate/20180103163649_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projects do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :author_id, null: false
+
+      t.timestamps
+    end
+    add_index :projects, :title
+    add_index :projects, :author_id
+  end
+end
