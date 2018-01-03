28dd2be2e03778d2aa68b11ee53680d98f28b322
Michael Berman <michael.v.berman@gmail.com>
2018-01-03 13:20:17 -0500

    add project ajax requests

---
+++ frontend/util/project_api_util.js
@@ -0,0 +1,27 @@
+
+export const fetchProjects = () => {
+  return $.ajax({
+    method: 'get',
+    url: 'api/projects'
+  });
+};
+
+export const fetchProject = id => {
+  return $.ajax({
+    method: 'get',
+    url: `api/projects/${id}`
+  });
+};
+
+export const createProject = project => {
+  return $.ajax({
+    method: 'post',
+    url: 'api/projects'
+  });
+};
+export const deleteProject = id => {
+  return $.ajax({
+    method: 'post',
+    url: `api/projects/${id}`
+  });
+};
