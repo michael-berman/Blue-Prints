64aed3250000d249b5cc4e0c9489619ae857bc04
Michael Berman <michael.v.berman@gmail.com>
2018-01-04 16:40:15 -0500

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> projects
 User.destroy_all
 Project.destroy_all
 
