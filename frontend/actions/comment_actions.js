0e427c247d416237f59eeb46ec18dd4041297db4
Michael Berman <michael.v.berman@gmail.com>
2018-01-09 16:17:45 -0500

    add comment action and api util

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,5 @@
+import * as CommentAPIUtil from '../util/comment_api_util';
+
+export const createComment = (comment) => dispatch => {
+  return CommentAPIUtil.createComment(comment);
+};
--- frontend/util/comment_api_util.js
+++ frontend/util/comment_api_util.js
@@ -2,7 +2,8 @@ export const createComment = comment => {
   return (
     $.ajax({
       method: 'post',
-      
+      url: 'api/projects/:project_id/comments',
+      data: { comment }
     })
   )
 }
