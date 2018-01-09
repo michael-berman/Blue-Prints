8971968a3c01ec0f7f1816993cc4ffb529877366
Michael Berman <michael.v.berman@gmail.com>
2018-01-08 20:22:10 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,67 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 User.destroy_all
 Project.destroy_all
 
-u1 = User.create!({email: 'mike@example.com', username: 'guest demo', password: 'hunter12'})
-u2 = User.create!({email: 'hunter12@example.com', username: 'hunter12', password: 'hunter12'})
-u3 = User.create!({email: 'starwars@example.com', username: 'starwars', password: 'starwars'})
-
-p1 = Project.create!({ title: "how to build a house", body:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum.", author_id: u1.id })
-p2 = Project.create!({ title: "how to build a shed", body:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-  culpa qui officia deserunt mollit anim id est laborum.", author_id: u2.id })
-p3 = Project.create!({ title: "how to build the death star", body:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum.", author_id: u3.id })
-p4 = Project.create!({ title: "how to build a car", body:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum.", author_id: u1.id })
-p5 = Project.create!({ title: "how to build a desk", body:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum.", author_id: u2.id })
-p6 = Project.create!({ title: "how to build the garage", body:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum.", author_id: u3.id })
+require_relative './seeds/user_seeding.rb'
+require_relative './seeds/projects_seeding'
 
 
-s1 = Step.create!({ title: "step1", body: "this is the body for step 1",
-  project_id: p1.id})
-s2 = Step.create!({ title: "step2", body: "this is the body for step 2",
-  project_id: p1.id})
-s3 = Step.create!({ title: "step3", body: "this is the body for step 3",
-  project_id: p1.id})
+u1 = User.create!({email: "mike@example.com", username: "guest demo", password: "hunter12"})
--- db/seeds/user_seeding.rb
+++ db/seeds/user_seeding.rb
@@ -1,34 +1,34 @@
 require 'faker'
 
 MALE__PROFILE_PICTURES = [
-  'photos/images/profile-picture-seeds/profile_pics/annette-keys-42075.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/felix-russell-saw-113844.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/daniele-levis-pelusi-385076.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/erwan-hesry-99395.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/ian-dooley-407897.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/jessy-paston-127537.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/joshua-sazon-373580.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/katie-treadway-171141.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/luke-porter-98215.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/matheus-ferrero-350443.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/natalie-pedigo-306019.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/nathaniel-kohfield-337185.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/nick-karvounis-142021.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/paulo-brandao-1001.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/priscilla-du-preez-318420.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/remi-skatulski-101224.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/roland-seifert-16528.jpg',
-  'photos/images/profile-picture-seeds/profile_pics/sam-manns-378189.jpg'
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/annette-keys-42075.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/felix-russell-saw-113844.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/daniele-levis-pelusi-385076.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/erwan-hesry-99395.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/ian-dooley-407897.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/jessy-paston-127537.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/joshua-sazon-373580.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/katie-treadway-171141.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/luke-porter-98215.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/matheus-ferrero-350443.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/natalie-pedigo-306019.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/nathaniel-kohfield-337185.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/nick-karvounis-142021.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/paulo-brandao-1001.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/priscilla-du-preez-318420.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/remi-skatulski-101224.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/roland-seifert-16528.jpg',
+  'https://s3.amazonaws.com/BLUE-PRINTS-DEV/photos/images/profile-picture-seeds/profile_pics/sam-manns-378189.jpg'
 ]
 
 # TODO: add background pics to each user
 
-demoUser = User.create!({email: 'mike@example.com', username: 'guest demo', password: 'hunter12'})
+User.create!({email: 'mike@example.com', username: 'guest demo', password: 'hunter12'})
 
 20.times do
   User.create!({
-    username: Faker::Name.name.join,
-    password: Faker::Name.name.join,
+    username: Faker::Name.name,
+    password: Faker::Name.name.split.join,
     email: Faker::Internet.email
   })
 end
