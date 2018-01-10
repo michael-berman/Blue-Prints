8324022c021a1b5714c618ddb8c7e62b1e87a8b2
Michael Berman <michael.v.berman@gmail.com>
2018-01-10 15:43:50 -0500

    add routes for search

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
-    resources :projects, only: [:create, :show, :index, :destroy] do
+    resources :projects, only: [:create, :show, :index, :destroy, :search] do
       resources :comments, only: [:create]
     end
     resources :steps, only: [:create, :index]
