499c623abbf5a339cf8ae4c17e508102397ee70f
Michael Berman <michael.v.berman@gmail.com>
2018-02-05 16:31:48 -0500

    delete indents

--- frontend/util/quill_toolbar_config.js
+++ frontend/util/quill_toolbar_config.js
@@ -1,8 +1,7 @@
 export const quillModules = {
   toolbar: [
     ['bold', 'italic', 'underline'],
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-    [{ 'indent': '-1'}, { 'indent': '+1' }]
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }]
   ]
 }
 
