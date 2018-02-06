dea3b4e4c96af440fe76c177fd0c6b913634c73d
Michael Berman <michael.v.berman@gmail.com>
2018-02-06 11:06:01 -0500

    add controller actions for favorites

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,2 +1,13 @@
 class Api::FavoritesController < ApplicationController
+
+  def create
+    @favorite = Favorite.new
+    @favorite.author = 
+  end
+
+  def destroy
+    favorite = Favorite.find(params[:id])
+    favorite.destroy
+  end
+
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -10,11 +10,11 @@
 #
 
 class Favorite < ApplicationRecord
-  belongs_to :author_id,
+  belongs_to :author,
     class_name: 'User',
     foreign_key: :author_id
 
-  belongs_to :project_id,
+  belongs_to :project,
     class_name: 'Project',
     foreign_key: :project_id
 
